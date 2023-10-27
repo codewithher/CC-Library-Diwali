2098e41a6eb413dc8c56e7f388f297e4e9d302b0
Higgins-Codes <30853424+totally-not-frito-lays@users.noreply.github.com>
2023-10-27 10:14:30 -0700

    i actually have no idea what is going on

--- examples/simple/simple.ino
+++ examples/simple/simple.ino
@@ -1,47 +1,42 @@
-// NeoPixel Ring simple sketch (c) 2013 Shae Erisson
-// Released under the GPLv3 license to match the rest of the
-// Adafruit NeoPixel library
-
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
  #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
 #endif
 
 // Which pin on the Arduino is connected to the NeoPixels?
-#define PIN        6 // On Trinket or Gemma, suggest changing this to 1
+#define PIN        6
 
 // How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS 16 // Popular NeoPixel ring size
+#define NUMPIXELS 1   // number of lights
 
 // When setting up the NeoPixel library, we tell it how many pixels,
 // and which pin to use to send signals. Note that for older NeoPixel
-// strips you might need to change the third parameter -- see the
-// strandtest example for more information on possible values.
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-#define DELAYVAL 500 // Time (in milliseconds) to pause between pixels
+// Time (in milliseconds) to pause between pixels
+// 500 milliseconds = 0.5s
+#define DELAYVAL 500 
 
 void setup() {
-  // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
-  // Any other board, you can remove this part (but no harm leaving it):
-#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
-  clock_prescale_set(clock_div_1);
-#endif
-  // END of Trinket-specific code.
-
-  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  // INITIALIZE NeoPixel strip object (REQUIRED)
+  pixels.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
+  pixels.begin();
 }
 
 void loop() {
-  pixels.clear(); // Set all pixel colors to 'off'
+  // Set all pixel colors to 'off'
+  pixels.clear();
 
   // The first NeoPixel in a strand is #0, second is 1, all the way up
   // to the count of pixels minus one.
   for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
 
-    // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
-    // Here we're using a moderately bright green color:
-    pixels.setPixelColor(i, pixels.Color(0, 150, 0));
+    // Set color values here! Colors range from 0 to 255
+    int Red = 0;
+    int Green = 150;
+    int Blue = 0;
+    
+    pixels.setPixelColor(i, pixels.Color(Red, Green, Blue));
 
     pixels.show();   // Send the updated pixel colors to the hardware.
 
