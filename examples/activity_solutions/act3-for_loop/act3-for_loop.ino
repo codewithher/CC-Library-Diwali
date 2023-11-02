860e6f85c2b174591e3fa211d1101dac83e17cc2
angelinamkr <82286088+angelinamkr@users.noreply.github.com>
2023-11-02 01:49:28 -0700

    adding function headers to act3

--- examples/activity_solutions/act3-for_loop/act3-for_loop.ino
+++ examples/activity_solutions/act3-for_loop/act3-for_loop.ino
@@ -20,7 +20,7 @@ void setup() {
 
 void loop() {
   changeRed(5, 10);
-  blinkingPatterns(2);
+  blinkingPatterns(2, 10);
   fadeInAndOut(4,5);
 }
 
@@ -29,6 +29,18 @@ void loop() {
 
 // TODO: paramterize functions
 // TODO: comment how to use the functions defined below
+
+
+/*
+* Parameters:
+*   - "seconds": a number that describes how long you want each "iteration" to be
+*   - "numSteps": a number that describes how many times you want the red color to change
+*
+* Function Description: This function sets your LED to white (aka all RGB values to 255) and decreases 
+*   the red RGB value by 10 a "numSteps" amount of times. It will decrease the value every "seconds" seconds.
+* 
+* Example Function Call: changeRed(2,4) --> this decreases the red RBG value every 2 seconds, for 4 times
+*/
 void changeRed(int seconds, int numSteps) {
   int Red = 255;
   int Green = 255;
@@ -42,6 +54,16 @@ void changeRed(int seconds, int numSteps) {
   }
 }
 
+/*
+* Parameters:
+*   - "seconds": a number that describes how long you want each "iteration" to be
+*   - "numSteps": a number that describes how many times you want the green color to change
+*
+* Function Description: This function sets your LED to white (aka all RGB values to 255) and decreases 
+*   the green RGB value by 10 a "numSteps" amount of times. It will decrease the value every "seconds" seconds.
+* 
+* Example Function Call: changeGreen(2,4) --> this decreases the green RBG value every 2 seconds, for 4 times
+*/
 void changeGreen(int seconds, int numSteps) {
   int Red = 255;
   int Green = 255;
@@ -55,6 +77,16 @@ void changeGreen(int seconds, int numSteps) {
   }
 }
 
+/*
+* Parameters:
+*   - "seconds": a number that describes how long you want each "iteration" to be
+*   - "numSteps": a number that describes how many times you want the blue color to change
+*
+* Function Description: This function sets your LED to white (aka all RGB values to 255) and decreases 
+*   the blue RGB value by 10 a "numSteps" amount of times. It will decrease the value every "seconds" seconds.
+* 
+* Example Function Call: changeBlue(2,4) --> this decreases the blue RBG value every 2 seconds, for 4 times
+*/
 void changeBlue(int seconds, int numSteps) {
   int Red = 255;
   int Green = 255;
@@ -68,11 +100,20 @@ void changeBlue(int seconds, int numSteps) {
   }
 }
 
-void blinkingPatterns(int seconds) {
+/*
+* Parameters:
+*   - "seconds": a number that describes how long you want the LED to stay on and off each blink
+*   - "num_blinks": a number that describes how many times you want the LED to blink
+*
+* Function Description: This function makes your LED blink "num_blinks" amount of times with the LED on and
+*   off for "seconds" amount of seconds.
+* 
+* Example Function Call: blinkingPatterns(1,10) --> this makes the LED blink on and off for 1 second each, 10 times
+*/
+void blinkingPatterns(int seconds, int num_blinks) {
   int Red = 255;
   int Green = 255;
   int Blue = 255;
-  int num_blinks = 10;
   int on_time = DELAYVAL * 2 * seconds;
   int off_time = DELAYVAL * 2 * seconds;
 
@@ -89,11 +130,30 @@ void blinkingPatterns(int seconds) {
   }
 }
 
+/*
+* Parameters:
+*   - "fadeInSeconds": a number that describes how long it takes for the LED to increase in brightness a little more
+*   - "fadeOutSeconds": a number that describes how long it takes for the LED to decrease in brightness a little more
+*
+* Function Description: This function makes your LED fade in with the brightness changing every "fadeInSeconds"
+*   seconds and then fade out with the brightness changing every "fadeOutSeconds" seconds
+* 
+* Example Function Call: fadeInAndOut(1,2) --> this makes the LED fade in with the brightness chaning every 1 second
+*   and then fade out with the brighness changing every 2 seconds
+*/
 void fadeInAndOut(int fadeInSeconds, int fadeOutSeconds) {
   fadeIn(fadeInSeconds);
   fadeOut(fadeOutSeconds);
 }
 
+/*
+* Parameters:
+*   - "seconds": a number that describes how long it takes for the LED to fade brightness a little more
+*
+* Function Description: This function makes your LED slowly get brighter each "seconds" number of seconds
+* 
+* Example Function Call: fadeIn(1) --> this increases the brightness a little every second
+*/
 void fadeIn(int seconds) {
   int Red = 150;
   int Green = 0;
@@ -106,6 +166,14 @@ void fadeIn(int seconds) {
   }
 }
 
+/*
+* Parameters:
+*   - "seconds": a number that describes how long it takes for the LED to fade brightness a little more
+*
+* Function Description: This function makes your LED slowly get darker each "seconds" number of seconds
+* 
+* Example Function Call: fadeOut(1) --> this decreases the brightness a little every second
+*/
 void fadeOut(int seconds) {
   int Red = 150;
   int Green = 0;
@@ -120,6 +188,17 @@ void fadeOut(int seconds) {
   }
 }
 
+/*
+* Parameters:
+*   - "seconds": a number that describes how long it takes for the LED to fade brightness a little more
+*   - "numPulses": how many times you want the LED to fade in and out
+*
+* Function Description: This function makes your LED slowly turn all the way on and all the way off a "numPulses"
+*   amount of times, with the brightness changing every "seconds" amount of seconds
+* 
+* Example Function Call: pulsatingEffect(1,2) --> this makes the LED turn on and off 2 times with the brightness
+*   changing every 1 second
+*/
 void pulsatingEffect(int seconds, int numPulses) {
   int Red = 150;
   int Green = 0;
